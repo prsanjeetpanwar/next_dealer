8aaaedf93a039b740b2dd44715d4796f93a74d06
prsanjeet panwar <prsanjeetpanwar729@gmail.com>
2023-07-03 16:18:00 +0530

    new update

--- src/app/delar/ragister/page.tsx
+++ src/app/delar/ragister/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 import React, { useState } from 'react';
-import { Card, Form,Input, Button } from 'antd';
-import { UserOutlined, BankOutlined, ShopOutlined, FileOutlined } from '@ant-design/icons';
+import { Card, Form,Menu, Progress } from 'antd';
+
+
+const { SubMenu } = Menu;
 
 const RightBoxForm = () => {
+  const [currentMenu, setCurrentMenu] = useState('registeredAddress');
   const [values, setValues] = useState({
     registeredAddress: '',
     contactPersonDetails: '',
@@ -22,101 +25,83 @@ const RightBoxForm = () => {
     documents: '',
   });
 
-  const onFinish = (values) => {
-    setValues(values);
+  const handleMenuClick = (e) => {
+    setCurrentMenu(e.key);
   };
 
-  const roundedRightIcon = (iconComponent, color) => (
-    <span style={{ color, paddingRight: 8 }}>{iconComponent}</span>
-  );
-
+  const handleFormSubmit = (values) => {
+    setValues(values);
+  };
+  const calculateProgress = () => {
+    const totalFields = Object.keys(values).length;
+    const completedFields = Object.values(values).filter((value) => value !== '').length;
+    const progressPercentage = (completedFields / totalFields) * 100;
+    return Math.round(progressPercentage);
+  };
   return (
-    <Card
-      title="Dealer Details"
-      style={{ width: 400 }}
-      headStyle={{ background: '#9542ef', color: 'white' }}
-    >
-      <Form onFinish={onFinish}>
-      <Form.Item
-  label="Registered Address"
-  name="registeredAddress"
-  rules={[{ required: true, message: 'Please enter the registered address' }]}
->
-  
-</Form.Item>
-        <Form.Item
-          label="Contact Person Details"
-          name="contactPersonDetails"
-          suffix={roundedRightIcon(<UserOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Bank Details"
-          name="bankDetails"
-          suffix={roundedRightIcon(<BankOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Godown Details"
-          name="godownDetails"
-          suffix={roundedRightIcon(<ShopOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Nearest Warehouse"
-          name="nearestWarehouse"
-          suffix={roundedRightIcon(<ShopOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Details of Property of Proprietor/Partner/Firm"
-          name="propertyDetails"
-          suffix={roundedRightIcon(<FileOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Registration Details"
-          name="registrationDetails"
-          suffix={roundedRightIcon(<FileOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Financial Information"
-          name="financialInformation"
-          suffix={roundedRightIcon(<FileOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Income Tax/ GST/Other"
-          name="taxInformation"
-          suffix={roundedRightIcon(<FileOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Fertilizer Sale"
-          name="fertilizerSale"
-          suffix={roundedRightIcon(<FileOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Agro Inputs"
-          name="agroInputs"
-          suffix={roundedRightIcon(<FileOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Sales Commitment"
-          name="salesCommitment"
-          suffix={roundedRightIcon(<FileOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Business Operations"
-          name="businessOperations"
-          suffix={roundedRightIcon(<FileOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Current dealership details / Fertilizer/seeds/Pesticides/Others"
-          name="currentDealership"
-          suffix={roundedRightIcon(<FileOutlined />, '#9542ef')}
-        ></Form.Item>
-        <Form.Item
-          label="Documents at row"
-          name="documents"
-          suffix={roundedRightIcon(<FileOutlined />, '#9542ef')}
-        ></Form.Item>
-       
-      </Form>
-    </Card>
+    <div style={{ display: 'flex', height: '50vw' }}>
+      <Menu
+        mode="vertical"
+        theme="light"
+        style={{ width: 250 }}
+        selectedKeys={[currentMenu]}
+        onClick={handleMenuClick}
+      >
+        <Menu.Item
+          key="registeredAddress"
+          style={{ height: 50, background: '#9542ef', color: 'white', display: 'flex', alignItems: 'center' }}
+        >
+          <h3 style={{ margin: 10, marginRight: 9, marginTop:30 }}>Form</h3>
+          <div>
+          <Progress percent={calculateProgress()} size="small" />
+          </div>
+          
+        </Menu.Item>
+        <Menu.Item key="registeredAddress">Registered Address</Menu.Item>
+        <Menu.Item key="contactPersonDetails">Contact Person Details</Menu.Item>
+        <Menu.Item key="bankDetails">Bank Details</Menu.Item>
+        <Menu.Item key="godownDetails">Godown Details</Menu.Item>
+        <Menu.Item key="nearestWarehouse">Nearest Warehouse</Menu.Item>
+        <Menu.Item key="propertyDetails">
+          Details of Property of Proprietor/Partner/Firm
+        </Menu.Item>
+        <Menu.Item key="registrationDetails">Registration Details</Menu.Item>
+        <Menu.Item key="financialInformation">Financial Information</Menu.Item>
+        <Menu.Item key="taxInformation">Income Tax/ GST/Other</Menu.Item>
+        <Menu.Item key="fertilizerSale">Fertilizer Sale</Menu.Item>
+        <Menu.Item key="agroInputs">Agro Inputs</Menu.Item>
+        <Menu.Item key="salesCommitment">Sales Commitment</Menu.Item>
+        <Menu.Item key="businessOperations">Business Operations</Menu.Item>
+        <Menu.Item key="currentDealership">
+          Current dealership details / Fertilizer/seeds/Pesticides/Others
+        </Menu.Item>
+        <Menu.Item key="documents">Documents</Menu.Item>
+      </Menu>
+      <Card
+        title="Dealer Details"
+        style={{ flex: 1, marginLeft: 16 }}
+        headStyle={{ background: '#9542ef', color: 'white' }}
+      >
+        <Form onFinish={handleFormSubmit}>
+          {currentMenu === 'registeredAddress' && (
+            <Form.Item
+              label="Registered Address"
+              name="registeredAddress"
+              rules={[{ required: true, message: 'Please enter the registered address' }]}
+            >
+            </Form.Item>
+          )}
+          {currentMenu === 'contactPersonDetails' && (
+            <Form.Item label="Contact Person Details" name="contactPersonDetails">
+            </Form.Item>
+          )}
+          {currentMenu === 'bankDetails' && (
+            <Form.Item label="Bank Details" name="bankDetails">
+            </Form.Item>
+          )}
+        </Form>
+      </Card>
+    </div>
   );
 };
 
